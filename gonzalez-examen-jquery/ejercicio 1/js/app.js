235015779ddd7e0098deb021300bd2ddc0946c0a
Traknous <yoryogon2@gmail.com>
2019-01-24 19:01:08 +0100

    examen jquery

--- gonzalez-examen-jquery/ejercicio 1/js/app.js
+++ gonzalez-examen-jquery/ejercicio 1/js/app.js
@@ -1,20 +1,13 @@
-function validar(){
-    var nombre = document.forms["registro"]["nombre"];
-    var apellidos = document.forms["registro"]["apellido"];
-    var nacimiento = document.forms["registro"]["nacimiento"];
-    if(nombre.value==""){
-        nombre.focus();
-        nombre.style.border = "1px solid red";
-        return false;
-    } else if(apellidos.value==""){
-        apellidos.focus();
-        apellidos.style.border = "1px solid red";
-        return false;
-    } else if(nacimiento.value==""){
-        nacimiento.focus();
-        nacimiento.style.border = "1px solid red";
-        return false;
-    } else {
-        alert("El formulario es correcto");
-    }
-};
+$(document).ready(function(){
+   
+    $("#registro").submit(function(e){
+        e.preventDefault();
+        var nombre = $("#nombre").val();
+        var apellidos = $("#apellidos").val();
+        var nacimiento = $("#nacimiento").val();
+        
+
+        
+    });
+
+});
